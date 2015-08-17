4c9faa535635aea9ead2670bbdeefb7287f42a14
davidvg <davidvazquezgijon@gmail.com>
2015-08-17 20:11:19 +0200

    Code cleaned in the .cpp file for the SkyPointer_MotorShield and SkyPointer_MicroStepper classes.

--- SkyPointer_MotorShield.cpp
+++ SkyPointer_MotorShield.cpp
@@ -12,10 +12,21 @@
  Written by Limor Fried/Ladyada for Adafruit Industries.
  BSD license, check license.txt for more information.
  All text above must be included in any redistribution.
- ******************************************************************/
+/******************************************************************************/
+    
+/*******************************************************************************
+    TO-DO
+    -----
+[ ] Make setSpeed functional or remove it. It should set the time interval in
+    the TimerOne interruption, but then it would not make sense when the library
+    is not used.
+[ ] Implement another 'getSpeed' function that returns actual speed instead of 
+    time interval?
 
+*******************************************************************************/
 /*
-    * microstepcurve[] changed from uint8 to uint16
+
+    ADD COMMENTS HERE, BIATCH !!
 
 */
 
@@ -33,71 +44,81 @@
  #define WIRE Wire1
 #endif
 
+// Custom macro for modulus function that allows negative numbers
+#define MOD(a,b) ((((a)%(b))+(b))%(b))
 
-#if (MICROSTEPS == 8)
+// Define the curve of amplitudes for the senoids to apply to the coils for 
+// each microstep
+#if (MICROSTEPS == 8)       //  8 microsteps per step
 uint16_t microstepcurve[] = {0, 799, 1567, 2275, 2896, 3405, 3783, 4016, 4095};
-#elif (MICROSTEPS == 16)
-//uint8_t microstepcurve[] = {0, 25, 50, 74, 98, 120, 141, 162, 180, 197, 212, 225, 236, 244, 250, 253, 255};
+#elif (MICROSTEPS == 16)    // 16 microsteps per step
 uint16_t microstepcurve[] = {0, 401, 799, 1189, 1567, 1930, 2275, 2598, 2896, 3165, 3405, 3611, 3783, 3919, 4016, 4075, 4095};
 #endif
 
+/******************************************************************************/
 SkyPointer_MotorShield::SkyPointer_MotorShield(uint8_t addr) {
-  _addr = addr;
-  _pwm = Adafruit_PWMServoDriver(_addr);
+    // Constructor for the class
+    _addr = addr;
+    _pwm = Adafruit_PWMServoDriver(_addr);
 }
-
+/******************************************************************************/
 void SkyPointer_MotorShield::begin(uint16_t freq) {
-  // init PWM w/_freq
-  WIRE.begin();
-  _pwm.begin();
-  _freq = freq;
-  _pwm.setPWMFreq(_freq);  // This is the maximum PWM frequency
-  for (uint8_t i=0; i<16; i++)
+    // Initializes the board with a PWM frequency of freq
+    WIRE.begin();
+    _pwm.begin();
+    _freq = freq;
+    _pwm.setPWMFreq(_freq);  // This is the maximum PWM frequency
+    for (uint8_t i=0; i<16; i++)
     _pwm.setPWM(i, 0, 0);
 }
-
+/******************************************************************************/
 void SkyPointer_MotorShield::setPWM(uint8_t pin, uint16_t value) {
-  if (value > 4095) {
+    // Sets PWM values for the coils excitation
+    if (value > 4095) {
     _pwm.setPWM(pin, 4096, 0);
-  } else
+    } else
     _pwm.setPWM(pin, 0, value);
 }
+/******************************************************************************/
 void SkyPointer_MotorShield::setPin(uint8_t pin, boolean value) {
-  if (value == LOW)
+    // Sets amplitude values for the coils excitation
+    if (value == LOW)
     _pwm.setPWM(pin, 0, 0);
-  else
+    else
     _pwm.setPWM(pin, 4096, 0);
 }
-/****************************************************************************/
-// Class for the MicroStepper motor
-// MicroStepper Motor
+/*******************************************************************************
+    ###  Class for the MicroStepper motor  ###
+*******************************************************************************/
 SkyPointer_MicroStepper::SkyPointer_MicroStepper (void) {
-    target = 0;
-    microstepsPerRev = 0;
-    microsteppernum = 0;
-    currMicrostep = 0;    // Current microstep
-    currPos = 0;          // Current position in microsteps
+    // Constructor for the MicroStepper class
+    // Initializes to zero
+    target = 0;           // Value for target position
+    microstepsPerRev = 0; // Steps per rev times MICROSTEPS
+    microsteppernum = 0;  // Identifier for the motor, {1, 2}
+    currMicrostep = 0;    // Current microstep in the range [0, MICROSTEPS]
+    currPos = 0;          // Current position in microsteps in the revolution
 }
-
+/******************************************************************************/
 SkyPointer_MicroStepper *SkyPointer_MotorShield::getMicroStepper (uint16_t steps, uint8_t num) {
     /*
     Defines a stepper motor in microstep rotation mode.
-        steps     --> Number of steps per revolution
-        numUSteps --> Number of microsteps per step --> FUTURE IMPLEMENTATION
-        num       --> Number assigned to the motor = {1, 2}
+        steps --> Number of steps per revolution
+        num   --> Number assigned to the motor = {1, 2}
     */
-    if (num > 2) return NULL;
-    num--;  // Changes the number of the motor to the range [0, 1]
+    if (num > 2) return NULL; // Motor number cannot be greater than 2 !!
+    num--;  // Changes the number of the motor to fit the range {0, 1}
 
     if (microsteppers[num].microsteppernum == 0) {
         microsteppers[num].microsteppernum = num; // Number of the motor
-        microsteppers[num].microstepsPerRev = steps*MICROSTEPS; //number of microsteps per rev
-                     // this could be changed to customize number of microsteps per step
+        microsteppers[num].microstepsPerRev = steps*MICROSTEPS; // Number of
+                   // microsteps per rev; this could be changed to customize the
+                   // number of microsteps per step by using a variable         
         microsteppers[num].MC = this;
         // Variables for the PWM control
         uint8_t pwma, pwmb, ain1, ain2, bin1, bin2;
         
-        // Configures the pins for the motor
+        // Configuration of the motor's pins
         if (num == 0) {
             pwma = 8;   ain2 = 9;   ain1 = 10;
             pwmb = 13;  bin2 = 12;  bin1 = 11;
@@ -114,22 +135,26 @@ SkyPointer_MicroStepper *SkyPointer_MotorShield::getMicroStepper (uint16_t steps
     }
     return &microsteppers[num];
 }
-
-
-void SkyPointer_MicroStepper::setSpeed (float rpm) { // -> Loads Timer values for ISR
+/******************************************************************************/
+void SkyPointer_MicroStepper::setSpeed (float rpm) {
     // This function calculates the value that must be loaded in
     // Timer1 register for interrupts to occur at needed intervals for producing
     // the desired speed
+    
+    // Currently it does nothing but calculate the time interval in microseconds
+    // between two microstep rotations in order to get the desired speed.
+    // Time interval still needs to be entered by hand in the Timer1 interruption
+    // definition.
     usecPerMicrostep = (uint32_t)(60000000L / (microstepsPerRev * rpm));
 }
-
-// Debug
+/******************************************************************************/
 uint32_t SkyPointer_MicroStepper::getSpeed() {
+    // Returns time interval in microsecons between two microstep rotations.
     return usecPerMicrostep;
 }
-
+/******************************************************************************/
 void SkyPointer_MicroStepper::release(void) {
-    // Releases the motor, removing every voltage from the coils. Free rotation.
+    // Releases the motor, removing voltage from the coils. Free rotation.
     MC->setPin(AIN1pin, LOW);
     MC->setPin(AIN2pin, LOW);
     MC->setPin(BIN1pin, LOW);
@@ -137,32 +162,38 @@ void SkyPointer_MicroStepper::release(void) {
     MC->setPWM(PWMApin, 0);
     MC->setPWM(PWMBpin, 0);
 }
-
+/******************************************************************************/
 void SkyPointer_MicroStepper::setTarget (uint16_t _target) {
-    // Sets the value for the target
+    // Sets the value for the target, in ABSOLUTE microsteps
     target = _target;
 }
-
+/******************************************************************************/
 boolean SkyPointer_MicroStepper::isTarget() {
-    // Checks if currPos == target and returns 1 if True or 0 if False
+    // Checks if currPos == target and returns 1 if True or 0 otherwise
     return (currPos == target);
 }
-
+/******************************************************************************/
 uint16_t SkyPointer_MicroStepper::microstep (uint16_t usteps, uint8_t dir) {
-    /* Produces a rotation of the specified microsteps in the given direction.
+    /* Produces a rotation of 'usteps' microsteps in the given direction.
             usteps --> Number of microsteps to rotate
             dir    --> Direction of the rotation = {FORWARD, BACKWARDS}
     */
     // Variables for the values to be loaded to the PWM registers
     uint16_t ocra, ocrb; // ocra, ocrb = [0, 4095] for the IC in the shield
-//    uint16_t numUSteps = microstepsPerRev*MICROSTEPS;
+    
     // Update current position of the rotor
-    if (dir == FORWARD) {
-        currMicrostep++;    // Forward rotation
-        currPos++;          // Update position
+    if (dir == FORWARD) {   // Forward rotation
+        currMicrostep++;
+        currPos++;
+        //  TEST !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // Make sure currPos is in range
+        currPos += microstepsPerRev;
         currPos %= microstepsPerRev;
     }
     else {
+    
+    // Can this be improved ????????????????????????????????????????????????????
+        
         if (currMicrostep == 0) {
             currMicrostep = 4*MICROSTEPS - 1; // Deals with negative values
         }
@@ -175,10 +206,12 @@ uint16_t SkyPointer_MicroStepper::microstep (uint16_t usteps, uint8_t dir) {
         else {
             currPos--;          // Update position
         }
+        //  TEST !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+        // Make sure currPos is in range
+        currPos += microstepsPerRev;        
         currPos %= microstepsPerRev;
-        //Serial.println(microstepsPerRev);
     }
-    // Make sure that currMicrosteps is in the right range: [0, 4*microsteps/rev]
+    // Make sure that currMicrostep is in the right range: [0, 4*MICROSTEPS]
     currMicrostep += 4*MICROSTEPS;
     currMicrostep %= 4*MICROSTEPS;
     
@@ -210,8 +243,8 @@ uint16_t SkyPointer_MicroStepper::microstep (uint16_t usteps, uint8_t dir) {
     MC -> setPWM(PWMApin, ocra);
     MC -> setPWM(PWMBpin, ocrb);
     
-    // Latch state for deciding on which coils to excite
-    uint8_t latch_state = 0;
+    // Latch state for deciding which coils to excite
+    uint8_t latch_state = 0;    // State variable
     
     if ((currMicrostep >= 0) && (currMicrostep < MICROSTEPS)) {
         latch_state |= 0x03;    // b'0011'
@@ -258,54 +291,19 @@ uint16_t SkyPointer_MicroStepper::microstep (uint16_t usteps, uint8_t dir) {
     
     return currPos;
 }
-
+/******************************************************************************/
 uint16_t SkyPointer_MicroStepper::getPosition() {
+    // Returns the current ABSOLUTE position of the motor.
     return currPos;
 }
+/******************************************************************************/
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/****************************************************************************/
-/****************************************************************************/
-/****************************************************************************/
-/****************************************************************************/
-/****************************************************************************/
-/****************************************************************************/
-/****************************************************************************/
-/****************************************************************************/
-/****************************************************************************/
-/****************************************************************************/
-/****************************************************************************/
-/****************************************************************************/
-
-
+/*******************************************************************************
+THIS PART OF THE CODE IS FOR NORMAL (ORIGINAL) MODE OF THE MOTOR, NOT MICROSTEP.
+*******************************************************************************/
+/*
 SkyPointer_StepperMotor *SkyPointer_MotorShield::getStepper(uint16_t steps, uint8_t num) {
   if (num > 2) return NULL;
 
@@ -333,26 +331,24 @@ SkyPointer_StepperMotor *SkyPointer_MotorShield::getStepper(uint16_t steps, uint
   }
   return &steppers[num];
 }
-
-
-/******************************************
-               STEPPERS
-******************************************/
-
-
-/*************************************************************************/
-
+*/
+/******************************************************************************/
+/*
 SkyPointer_StepperMotor::SkyPointer_StepperMotor(void) {
   revsteps = steppernum = currentstep = 0;
 }
-
+*/
+/******************************************************************************/
+/*
 void SkyPointer_StepperMotor::setSpeed(uint16_t rpm) {
   //Serial.println("steps per rev: "); Serial.println(revsteps);
   //Serial.println("RPM: "); Serial.println(rpm);
 
   steppingcounter = 0;
 }
-
+*/
+/******************************************************************************/
+/*
 void SkyPointer_StepperMotor::release(void) {
   MC->setPin(AIN1pin, LOW);
   MC->setPin(AIN2pin, LOW);
@@ -361,7 +357,8 @@ void SkyPointer_StepperMotor::release(void) {
   MC->setPWM(PWMApin, 0);
   MC->setPWM(PWMBpin, 0);
 }
-
+*/
+/******************************************************************************/
 /*
 void SkyPointer_StepperMotor::microstep(uint16_t usteps, uint8_t dir) {
   uint32_t uspers = usperstep;
